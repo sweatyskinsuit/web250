517976a8f9e028c1d1506ce77d18ecbcb97b4476
Hanna Lesky <hannaalesk@students.abtech.edu>
2025-09-22 19:21:15 -0400

    Completed bird.php

---
+++ asgn03-static-challenge/Bird.php
@@ -0,0 +1,39 @@
+<?php
+
+class Bird
+{
+    public static $instance_count;
+    public static $egg_num = 0;
+    public $habitat;
+    public $food;
+    public $nesting = "tree";
+    public $conservation;
+    public $song = "chirp";
+    public $flying = "yes";
+
+    function can_fly()
+    {
+        return $this->flying == "yes" ? "can fly" : "is stuck on the ground";
+    }
+
+    public static function create()
+    {
+        self::$instance_count++;
+        return new Bird();
+    }
+}
+
+class YellowBelliedFlyCatcher extends Bird
+{
+    public static $egg_num = "3-4, sometimes 5.";
+    public $name = "yellow-bellied flycatcher";
+    public $diet = "mostly insects.";
+    public $song = "flat chilk";
+}
+
+class Kiwi extends Bird
+{
+    public $name = "kiwi";
+    public $diet = "omnivorous";
+    public $flying = "no";
+}
